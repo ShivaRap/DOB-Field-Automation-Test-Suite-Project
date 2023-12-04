1b16be1e543f58cd34751d8b77f32241b856a341
ShivaRap <rapolshiva@outlook.com>
2023-12-04 10:33:10 -0800

    src edit

--- src/test/java/com/spring/testing/EmailValidationTest.java
+++ src/test/java/com/spring/testing/EmailValidationTest.java
@@ -14,7 +14,7 @@ public class EmailValidationTest extends ContactFormTest {
             {"chistopher.nolan@domain", false},   // Invalid email (no Top Level Domain)
             {"@example.com", false},        // Invalid email (no username)
             {"plainaddress", false},        // Invalid email (no @ or domain)
-            {".priyanka@domain.com", false},    // Invalid email (leading dot)
+            {".agasthya@domain.com", false},    // Invalid email (leading dot)
             {"shri@domain..com", false},    // Invalid email (double dot)
             {"user@123.com", true},          // Valid email with numerical domain
             {"user@123", false}              // Invalid email with missing Top Level Domain in numerical domain
